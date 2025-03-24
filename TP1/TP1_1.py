173765d1fa343a55980e282e284aea0d30b1fb4f
Germán Kousal <germankousal@protonmail.com>
2025-03-24 20:48:07 -0300

    Chequeando si el arreglo pasa por valor o referencia

--- TP1/TP1_1.py
+++ TP1/TP1_1.py
@@ -11,5 +11,21 @@ class Orden(ABC):
 class OrdenAscendente(Orden):
 
     def ordenar(self, arreglo_a_ordenar):
+        print(id(arreglo_a_ordenar))
         arreglo_a_ordenar.sort()
 
+
+
+
+def main():
+    arreglo = [2,5,8,1,0,21]
+
+    ordenar = OrdenAscendente()
+    print(id(arreglo))
+    ordenar.ordenar(arreglo)
+
+    print(arreglo)
+    print('tu mama')
+
+
+main()
