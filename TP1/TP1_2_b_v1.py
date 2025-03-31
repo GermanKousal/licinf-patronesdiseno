87229360e9c2b35ff41fe97ec9f657247a2f70c8
Germán Kousal <germankousal@protonmail.com>
2025-03-30 21:00:18 -0300

    TP1_2: formateado con PEP8

--- TP1/TP1_2_b_v1.py
+++ TP1/TP1_2_b_v1.py
@@ -90,14 +90,12 @@ class Carrito:
     def agregar_producto(self, producto: Producto) -> None:
         nuevo_estado = self.__estado_actual.agregar_producto()
         self.__cambiar_estado(nuevo_estado)
-        
+
         if isinstance(self.__estado_actual, Activado):
             self.__productos.append(producto)
             print(f"Producto: {producto.nombre} agregado.")
         else:
             print("No se puede agregar producto en el estado actual.")
-
-
 
     def cancelar(self) -> None:
         self.__cambiar_estado(self.__estado_actual.cancelar())
@@ -107,7 +105,7 @@ class Carrito:
 
     def activar(self) -> None:
         self.__cambiar_estado(self.__estado_actual.activar())
-    
+
     # Private Methods
     def __cambiar_estado(self, nuevo_estado: Estado | None):
         if nuevo_estado is not None:
@@ -129,11 +127,13 @@ def main() -> None:
 
     print("\n*** Cambiar estado a Cancelado y agregar producto 2 ***")
     carrito.cancelar()  # Cambia el estado a Cancelado
-    carrito.agregar_producto(producto2)  # Debería activar el carrito y luego agregar producto 2
+    # Debería activar el carrito y luego agregar producto 2
+    carrito.agregar_producto(producto2)
 
     print("\n*** Cambiar estado a Pagado y agregar producto 3 ***")
     carrito.pagar()  # Cambia el estado a Pagado
-    carrito.agregar_producto(producto3)  # No debería permitir agregar productos
+    # No debería permitir agregar productos
+    carrito.agregar_producto(producto3)
 
     print("\n*** Cambiar estado a Activado y agregar producto 3 ***")
     carrito.activar()  # Cambia el estado a Activado
