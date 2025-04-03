263f5d57ce888a66ab65aa0ef9097a5082bcb3f3
Germán Kousal <germankousal@protonmail.com>
2025-04-02 22:52:22 -0300

    TP1_3: Agregada posibilidad de cerrar una conexion. Agregado software de testeo.

--- TP1/TP1_3_v1.py
+++ TP1/TP1_3_v1.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import random
 from abc import ABC, abstractmethod
 
 
@@ -7,12 +8,12 @@ class Conexion:
     __INSTANCIA_UNICA: Conexion | None = None
 
     def __new__(cls):
-        
+
         if cls.__INSTANCIA_UNICA is None:
             cls.__INSTANCIA_UNICA = super().__new__(cls)
-        
+
         return cls.__INSTANCIA_UNICA
-    
+
     def __init__(self) -> None:
 
         # Instance Attributes
@@ -20,9 +21,13 @@ class Conexion:
         self.__nombre_servidor: str = "nombre_servidor"
         self.__nombre_usuario: str = "nombre_usuario"
         self.__contrasenha: str = "contrasenha"
-        self.__nombre_DB: str = "nombre_DB"
+        self.__nombre_DB: str = str(random.getrandbits(32))
 
     # Methods
+
+    @property
+    def nombre_DB(self) -> str:
+        return self.__nombre_DB
 
     def create(self) -> None:
         pass
@@ -36,14 +41,24 @@ class Conexion:
     def delete(self) -> None:
         pass
 
-    @property
-    def nombre_DB(self) -> str:
-        return self.__nombre_DB
+    def close(self) -> None:
+        Conexion.__INSTANCIA_UNICA = None
 
 
 def main() -> None:
-    conexion = Conexion()
-    print(conexion.nombre_DB)
+    conexion1 = Conexion()
+    print(id(conexion1))
+
+    conexion2 = Conexion()
+    print(id(conexion2))
+
+    conexion1.close()
+
+    conexion1 = Conexion()
+    print(id(conexion1))
+
+    conexion2 = Conexion()
+    print(id(conexion2))
 
 
 if __name__ == "__main__":
