07866e55e3607cd13f599626d3a08fdac73b269c
Germán Kousal <germankousal@protonmail.com>
2025-04-02 22:33:25 -0300

    TP1_3: Implementado logica para unica instancia.

--- TP1/TP1_3_v1.py
+++ TP1/TP1_3_v1.py
@@ -6,14 +6,21 @@ class Conexion:
 
     __INSTANCIA_UNICA: Conexion | None = None
 
-    def __init__(self, nombre_servidor: str, nombre_usuario: str, contrasenha: str, nombre_DB: str) -> None:
+    def __new__(cls):
+        
+        if cls.__INSTANCIA_UNICA is None:
+            cls.__INSTANCIA_UNICA = super().__new__(cls)
+        
+        return cls.__INSTANCIA_UNICA
+    
+    def __init__(self) -> None:
 
         # Instance Attributes
 
-        self.__nombre_servidor: str = nombre_servidor
-        self.__nombre_usuario: str = nombre_usuario
-        self.__contrasenha: str = contrasenha
-        self.__nombre_DB: str = nombre_DB
+        self.__nombre_servidor: str = "nombre_servidor"
+        self.__nombre_usuario: str = "nombre_usuario"
+        self.__contrasenha: str = "contrasenha"
+        self.__nombre_DB: str = "nombre_DB"
 
     # Methods
 
@@ -29,9 +36,14 @@ class Conexion:
     def delete(self) -> None:
         pass
 
+    @property
+    def nombre_DB(self) -> str:
+        return self.__nombre_DB
+
 
 def main() -> None:
-    conexion = Conexion("Servidor K", "German", "123", "Canciones")
+    conexion = Conexion()
+    print(conexion.nombre_DB)
 
 
 if __name__ == "__main__":
