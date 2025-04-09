49b86b0d1533ff10a7515d57ab2548a4252f64cc
Germán Kousal <germankousal@protonmail.com>
2025-04-09 10:13:55 -0300

    TP1_4: Archivo creado.

---
+++ TP1/TP1_4_v1.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class ElementoSistema(ABC):
+    pass
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
