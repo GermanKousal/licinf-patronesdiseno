1f30eb4feb71d0e55643374c0047a69d23919fc1
Germán Kousal <germankousal@protonmail.com>
2025-04-09 12:06:30 -0300

    TP1_4: Agregado nombre y tamanio a clase ElementoSistema.

--- TP1/TP1_4_v1.py
+++ TP1/TP1_4_v1.py
@@ -3,7 +3,16 @@ from abc import ABC, abstractmethod
 
 
 class ElementoSistema(ABC):
-    pass
+
+    def __init__(self, nombre: str, tamaño: int) -> None:
+        self._nombre = nombre
+        self._tamaño: int = tamaño
+
+    def mostrar_nombre(self) -> str:
+        return self._nombre
+
+    def obtener_tamaño(self) -> int:
+        return self._tamaño
 
 
 class Archivo(ElementoSistema):
