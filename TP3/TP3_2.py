5123c019d90b90743387cfdd5d5fdcbd1e02327f
Germán Kousal <germankousal@protonmail.com>
2025-06-29 20:40:18 -0300

    TP3_2: Creada bosquejo inicial de clase Estado y CajaFuerte

--- TP3/TP3_2.py
+++ TP3/TP3_2.py
@@ -2,6 +2,21 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 
 
+class Estado(ABC):
+    def activar(self):
+        ...
+
+    def desactivar(self):
+        ...
+
+    def configurar(self):
+        ...
+
+
+class CajaFuerte:
+    ...
+
+
 def main() -> None:
     pass
 
