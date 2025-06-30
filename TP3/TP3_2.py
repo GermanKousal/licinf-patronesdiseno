cc99d2d59294e70a54323d53ff1c99563edb356a
Germán Kousal <germankousal@protonmail.com>
2025-06-29 22:48:45 -0300

    3.2 agregado tipos de retorno a metodos

--- TP3/TP3_2.py
+++ TP3/TP3_2.py
@@ -3,64 +3,64 @@ from abc import ABC
 
 
 class Estado(ABC):
-    def activar(self, caja_fuerte: CajaFuerte):
+    def activar(self, caja_fuerte: CajaFuerte) -> None:
         print("No se puede activar en este estado.")
 
-    def desactivar(self, caja_fuerte: CajaFuerte):
+    def desactivar(self, caja_fuerte: CajaFuerte) -> None:
         print("No se puede desactivar en este estado.")
 
-    def configurar(self, caja_fuerte: CajaFuerte):
+    def configurar(self, caja_fuerte: CajaFuerte) -> None:
         print("No se puede configurar en este estado.")
 
-    def _mensaje_cambio_estado(self, desde: str, hacia: str):
+    def _mensaje_cambio_estado(self, desde: str, hacia: str) -> None:
         print(f"{desde} -> {hacia}")
 
 
 class Activada(Estado):
-    def desactivar(self, caja_fuerte: CajaFuerte):
+    def desactivar(self, caja_fuerte: CajaFuerte) -> None:
         self._mensaje_cambio_estado("ACTIVADA", "DESACTIVADA")
         caja_fuerte._cambiar_estado(Desactivada())
 
 
 class Desactivada(Estado):
-    def activar(self, caja_fuerte: CajaFuerte):
+    def activar(self, caja_fuerte: CajaFuerte) -> None:
         self._mensaje_cambio_estado("DESACTIVADA", "ACTIVADA")
         caja_fuerte._cambiar_estado(Activada())
 
-    def configurar(self, caja_fuerte: CajaFuerte):
+    def configurar(self, caja_fuerte: CajaFuerte) -> None:
         self._mensaje_cambio_estado("DESACTIVADA", "CONFIGURACION")
         caja_fuerte._cambiar_estado(Configuracion())
 
 
 class Configuracion(Estado):
-    def activar(self, caja_fuerte: CajaFuerte):
+    def activar(self, caja_fuerte: CajaFuerte) -> None:
         self._mensaje_cambio_estado("CONFIGURACION", "ACTIVADA")
         caja_fuerte._cambiar_estado(Activada())
 
-    def desactivar(self, caja_fuerte: CajaFuerte):
+    def desactivar(self, caja_fuerte: CajaFuerte) -> None:
         self._mensaje_cambio_estado("CONFIGURACION", "DESACTIVADA")
         caja_fuerte._cambiar_estado(Desactivada())
 
 
 class CajaFuerte:
 
-    def __init__(self, PIN: int = 0):
+    def __init__(self, PIN: int = 0) -> None:
         self._estado: Estado = Desactivada()
         self._PIN: int = PIN
 
-    def activar(self):
+    def activar(self) -> None:
         self._estado.activar(self)
 
-    def desactivar(self):
+    def desactivar(self) -> None:
         self._estado.desactivar(self)
 
-    def configurar(self):
+    def configurar(self) -> None:
         self._estado.configurar(self)
 
-    def _cambiar_estado(self, estado: Estado):
+    def _cambiar_estado(self, estado: Estado) -> None:
         self._estado = estado
 
-    def _cambiar_PIN(self, PIN: int):
+    def _cambiar_PIN(self, PIN: int) -> None:
         self._PIN = PIN
 
 
